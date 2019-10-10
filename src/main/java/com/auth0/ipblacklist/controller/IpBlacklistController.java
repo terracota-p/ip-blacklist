85de69a8d83ae5ea4b41e9daa528e51fc356ea5f
c.friaszapater <c.friaszapater@orwellg.com>
2019-10-10 09:51:14 +0200

    controller

--- src/main/java/com/auth0/ipblacklist/controller/IpBlacklistController.java
+++ src/main/java/com/auth0/ipblacklist/controller/IpBlacklistController.java
@@ -1,22 +1,33 @@
 package com.auth0.ipblacklist.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class IpBlacklistController {
 
   @GetMapping("/status")
-  public Mono<String> status() {
-    return Mono.just("I'm alive");
+  public Mono<Void> status() {
+    return Mono.empty();
   }
 
-  @GetMapping("/ip/{ip}")
-  public Mono<String> ip() {
-    return Mono.just("I'm alive");
+  @GetMapping("/ips/{ip}")
+  public Mono<ResponseEntity<String>> ips(@PathVariable String ip) {
+    log.debug("GET ip {}", ip);
+    return Mono.just(new ResponseEntity<>(HttpStatus.NO_CONTENT));
   }
 
+  @PostMapping("/reload")
+  public Mono<Void> reload() {
+    return Mono.empty();
+  }
 }
--- src/test/java/com/auth0/ipblacklist/IpBlacklistApplicationTests.java
+++ src/test/java/com/auth0/ipblacklist/IpBlacklistApplicationTests.java
@@ -2,15 +2,35 @@ package com.auth0.ipblacklist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureWebTestClient
 public class IpBlacklistApplicationTests {
 
+  @Autowired
+  private WebTestClient webClient;
+
   @Test
-  public void contextLoads() {
+  public void WhenGetStatus_ThenOk() {
+    WebTestClient.ResponseSpec result = webClient.get().uri("http://localhost/status").exchange();
+    result.expectStatus().isOk();
   }
 
+  @Test
+  public void WhenPostReload_ThenOk() {
+    WebTestClient.ResponseSpec result = webClient.post().uri("http://localhost/reload").exchange();
+    result.expectStatus().isOk();
+  }
+
+  @Test
+  public void WhenGetIp_GivenNotBlacklistedIp_ThenNoContent() {
+    WebTestClient.ResponseSpec result = webClient.get().uri("http://localhost/ips/1.1.1.1").exchange();
+    result.expectStatus().isNoContent();
+  }
 }
