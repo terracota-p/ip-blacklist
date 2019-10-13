84dae4f7cdb3a1238d88487ec6eba7e8b90ecbc5
c.friaszapater <c.friaszapater@orwellg.com>
2019-10-13 19:14:33 +0200

    don't include null DTO values in response

--- src/main/java/com/auth0/ipblacklist/dto/PositiveResultMetadataDto.java
+++ src/main/java/com/auth0/ipblacklist/dto/PositiveResultMetadataDto.java
@@ -1,5 +1,6 @@
 package com.auth0.ipblacklist.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PositiveResultMetadataDto {
   private String blacklist;
   private String ip;
