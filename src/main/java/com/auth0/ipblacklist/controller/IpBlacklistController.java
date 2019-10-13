cbdb70a414e73204c769c1db1f111427228003a3
c.friaszapater <c.friaszapater@orwellg.com>
2019-10-13 18:43:03 +0200

    set media type of positive response

--- src/main/java/com/auth0/ipblacklist/controller/IpBlacklistController.java
+++ src/main/java/com/auth0/ipblacklist/controller/IpBlacklistController.java
@@ -7,6 +7,7 @@ import com.auth0.ipblacklist.service.IpBlacklistService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ public class IpBlacklistController {
     return Mono.empty();
   }
 
-  @GetMapping("/ips/{ip}")
+  @GetMapping(value = "/ips/{ip}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Mono<ResponseEntity<PositiveResultMetadataDto>> getIp(@PathVariable String ip) {
     log.debug("GET ip {}", ip);
 
