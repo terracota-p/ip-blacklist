f3e9fa34ee4421adbd45a4c0bd0dcabd00e1dbf3
c.friaszapater <c.friaszapater@orwellg.com>
2019-10-15 11:20:53 +0200

    refactor: organize

--- acceptance-tests/load-test.js
+++ acceptance-tests/load-test.js
@@ -26,63 +26,92 @@ async.mapLimit(
     // results is now an array of the responses
 
     const overallTime = new Date().getTime() - overallStartTime;
-
-    const requestsWithUnexpectedStatus = results.filter(
-      response => response.statusCode !== 200 && response.statusCode !== 204
-    );
-    const requestsOverMaxThreshold = results.filter(
-      response => response.elapsedTime > 200
-    );
-    const averageLatency =
-      results
-        .map(response => response.elapsedTime)
-        .reduce((accumulator, value) => accumulator + value) / requests;
-
-    console.log("average latency: " + averageLatency + "ms");
-    console.log(
-      "max latency: " +
-        results
-          .map(response => response.elapsedTime)
-          .reduce((accumulator, value) =>
-            value > accumulator ? value : accumulator
-          ) +
-        "ms"
-    );
     const requestsPerSecond = (requests * 1000) / overallTime;
-    console.log("Rate: " + Math.ceil(requestsPerSecond) + " requests/s");
-    console.log("Total requests processed: " + requests);
 
-    if (results.length !== requests) {
-      throw new Error(requests - results.length + "requests did not finish.");
-    }
+    const {
+      averageLatency,
+      requestsWithUnexpectedStatus,
+      requestsOverMaxThreshold
+    } = aggregateResults(results);
 
-    if (requestsWithUnexpectedStatus.length > 0) {
-      throw new Error(
-        requestsWithUnexpectedStatus.length +
-          " requests with unexpected status: " +
-          JSON.stringify(requestsWithUnexpectedStatus)
-      );
-    }
+    printResultsSummary(averageLatency, results, requestsPerSecond);
 
-    if (requestsOverMaxThreshold.length > 0) {
-      throw new Error(
-        requestsOverMaxThreshold.length + " requests over max threshold."
-      );
-    }
-
-    if (averageLatency > 50) {
-      throw new Error("Average latency over threshold: " + averageLatency);
-    }
-
-    if (requestsPerSecond < 900) {
-      throw new Error(
-        "Could not match desired rate of at least 900 requests/s: " +
-          requestsPerSecond
-      );
-    }
+    checkResults(
+      results,
+      requestsWithUnexpectedStatus,
+      requestsOverMaxThreshold,
+      averageLatency,
+      requestsPerSecond
+    );
   }
 );
 
 function randomIp() {
   return "0.0.0.0";
+}
+
+function aggregateResults(results) {
+  const requestsWithUnexpectedStatus = results.filter(
+    response => response.statusCode !== 200 && response.statusCode !== 204
+  );
+  const requestsOverMaxThreshold = results.filter(
+    response => response.elapsedTime > 200
+  );
+  const averageLatency =
+    results
+      .map(response => response.elapsedTime)
+      .reduce((accumulator, value) => accumulator + value) / requests;
+  return {
+    averageLatency,
+    requestsWithUnexpectedStatus,
+    requestsOverMaxThreshold
+  };
+}
+
+function printResultsSummary(averageLatency, results, requestsPerSecond) {
+  console.log("average latency: " + averageLatency + "ms");
+  console.log(
+    "max latency: " +
+      results
+        .map(response => response.elapsedTime)
+        .reduce((accumulator, value) =>
+          value > accumulator ? value : accumulator
+        ) +
+      "ms"
+  );
+  console.log("Rate: " + Math.ceil(requestsPerSecond) + " requests/s");
+  console.log("Total requests processed: " + requests);
+}
+
+function checkResults(
+  results,
+  requestsWithUnexpectedStatus,
+  requestsOverMaxThreshold,
+  averageLatency,
+  requestsPerSecond
+) {
+  if (results.length !== requests) {
+    throw new Error(requests - results.length + "requests did not finish.");
+  }
+  if (requestsWithUnexpectedStatus.length > 0) {
+    throw new Error(
+      requestsWithUnexpectedStatus.length +
+        " requests with unexpected status: " +
+        JSON.stringify(requestsWithUnexpectedStatus)
+    );
+  }
+  if (requestsOverMaxThreshold.length > 0) {
+    throw new Error(
+      requestsOverMaxThreshold.length + " requests over max threshold."
+    );
+  }
+  if (averageLatency > 50) {
+    throw new Error("Average latency over threshold: " + averageLatency);
+  }
+  if (requestsPerSecond < 900) {
+    throw new Error(
+      "Could not match desired rate of at least 900 requests/s: " +
+        requestsPerSecond
+    );
+  }
 }
